927f3c8e28ec6e7790fb70b757949312d8a237ce
Carl Jayson Angeles <73303615+cajakey@users.noreply.github.com>
2022-04-17 21:34:14 +0800

    circleimageview

--- Leafdex/app/src/main/java/com/example/leafdex/fragments/home.java
+++ Leafdex/app/src/main/java/com/example/leafdex/fragments/home.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -118,6 +119,7 @@ public class home extends Fragment {
                         post.add(childDataSnapshot.child("price").getValue().toString()); //post price
                         posts.add(post);
                     }
+                    Collections.reverse(posts);
 
                     //Addition of posts into recycler view
                     for(ArrayList<String> childPosts : posts){
